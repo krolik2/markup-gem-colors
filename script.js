bf2d3e30a3b5870dc7f9620aa39df46174a3c050
krolik <krolikmat@gmail.com>
2019-12-13 17:40:51 +0100

    added: darkmode, newcontent, refactor

--- script.js
+++ script.js
@@ -1,20 +1,78 @@
-let list = document.querySelectorAll("li");
+const list = document.querySelectorAll("strong");
+const listArr = Array.from(list);
+const darkIcon = document.querySelector(".dark");
+const lightIcon = document.querySelector(".light");
+const body = document.querySelector("body");
 
-let greenGems = ['explosive trap','lesser poison support','onslaught support','herald of ice','trap and mine damage support', 'added cold damage','chain support'];
-let blueGems = ['freezing pulse','orb of storms','elemental proliferation support','frost blink','arcane surge support','added lightning damage support','flame dash','arc','frost bomb','herald of thunder','wave of conviction','controlled destruction support','conductivity','lightning spire trap'];
+window.addEventListener("load", () => {
+  addBlue();
+  addGreen();
+  addRed();
+});
 
-function addColor (el) {
-    if  (greenGems.some(name => el.innerText.includes(name))) {
-        el.classList.add('green')
-    } else if (blueGems.some(name => el.innerText.includes(name))) {
-        el.classList.add('blue')
-    } else {
-        console.log('no match')
-    }
-}
+darkIcon.addEventListener("click", () => {
+  darkIcon.classList.add("invisible");
+  lightIcon.classList.remove("invisible");
+  body.classList.add("darkmode");
+});
 
+lightIcon.addEventListener("click", () => {
+  lightIcon.classList.add("invisible");
+  darkIcon.classList.remove("invisible");
+  body.classList.remove("darkmode");
+});
 
-for (let i = 0; i < list.length; i++) {
-    addColor(list[i]);
-}
+const greenGems = [
+  "explosive trap",
+  "lesser poison support",
+  "onslaught support",
+  "herald of ice",
+  "trap and mine damage support",
+  "added cold damage support",
+  "chain support",
+  "caustic arrow",
+  "pierce support",
+  "rain of arrows",
+  "mirage archer support",
+  "scourage arrow",
+  "lightning arrow",
+  "tornado shot"
+];
+const blueGems = [
+  "freezing pulse",
+  "orb of storms",
+  "elemental proliferation support",
+  "frost blink",
+  "arcane surge support",
+  "added lightning damage support",
+  "flame dash",
+  "arc",
+  "frost bomb",
+  "herald of thunder",
+  "wave of conviction",
+  "controlled destruction support",
+  "conductivity",
+  "lightning spire trap"
+];
+const redGems = ["elemental damage with attacks support"];
 
+const addBlue = () => {
+  const blueList = listArr.filter(element =>
+    blueGems.includes(element.innerText)
+  );
+  blueList.forEach(el => el.classList.add("blue"));
+};
+
+const addGreen = () => {
+  const greenList = listArr.filter(element =>
+    greenGems.includes(element.innerText)
+  );
+  greenList.forEach(el => el.classList.add("green"));
+};
+
+const addRed = () => {
+  const redList = listArr.filter(element =>
+    redGems.includes(element.innerText)
+  );
+  redList.forEach(el => el.classList.add("red"));
+};
